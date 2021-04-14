b91de83cc7b9afbf1018c2801d17f510b50569f1
William Lahti <wilahti@gmail.com>
2021-04-14 00:58:05 -0700

    add more tests

--- src/session-description.test.ts
+++ src/session-description.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { describe } from "razmin";
-import { SDP } from "./session-description";
+import { SDP, SessionDescription } from "./session-description";
 
 const EXAMPLE1 = `v=0
 o=jdoe 2890844526 2890842807 IN IP4 10.47.16.5
@@ -13,7 +13,7 @@ t=2873397496 2873404696
 a=recvonly
 m=audio 49170 RTP/AVP 0
 m=video 51372 RTP/AVP 99
-a=rtpmap:99 h263-1998/90000`;
+a=rtpmap:99 h263-1998/90000`.replace(/\n/g, "\r\n");
 
 describe('SDP', it => {
     it('parses a simple sdp', () => {
@@ -58,5 +58,20 @@ describe('SDP', it => {
         expect(sdp.media[1].formats).to.eql(['99']);
         expect(sdp.media[1].attributes[0].value).to.equal('99 h263-1998/90000');
         expect(sdp.media[1].attributes[0].value).to.equal('99 h263-1998/90000');
+
+        console.log(JSON.stringify(sdp));
+    });
+
+    it('should be tolerant of line endings', () => {
+        let sdp = SDP.parse(EXAMPLE1.replace(/\r\n/g, "\n"));
+        expect(sdp.uri).to.equal('http://www.example.com/seminars/sdp.pdf');
+    });
+
+    it('stringifies a basic example', () => {
+        let json : SessionDescription = JSON.parse(
+            `{"emails":[{"value":"j.doe@example.com","name":"Jane Doe"}],"phoneNumbers":[],"times":[{"startTime":2873397496,"stopTime":2873404696,"repeats":[]}],"timeZoneAdjustments":[],"bandwidth":[],"attributes":[{"name":"recvonly"}],"media":[{"type":"audio","port":49170,"numberOfPorts":1,"transport":"RTP/AVP","formats":["0"],"attributes":[],"bandwidth":[],"connections":[],"title":null},{"type":"video","port":51372,"numberOfPorts":1,"transport":"RTP/AVP","formats":["99"],"attributes":[{"name":"rtpmap","value":"99 h263-1998/90000"}],"bandwidth":[],"connections":[],"title":null}],"version":0,"origin":{"username":"jdoe","sessionID":2890844526,"version":2890842807,"networkType":"IN","addressType":"IP4","address":"10.47.16.5"},"sessionName":"SDP Seminar","information":"A Seminar on the session description protocol","uri":"http://www.example.com/seminars/sdp.pdf","connection":{"addressType":"IP4","address":"224.2.17.12","networkType":"IN","layerCount":1,"timeToLive":127}}`
+        );
+        let sdp = SDP.stringify(json);
+        expect(sdp).to.equal(EXAMPLE1);
     });
 });
